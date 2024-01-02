3e7970882bfdb014b0735b4b11ffca7324a7b9d5
Leszek Chojnacki <leszekchojnacki@gmail.com>
2024-01-02 21:26:47 +0100

    Initial commit

---
+++ src/test/java/pl/nowekolory/REST/RestApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.nowekolory.REST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
